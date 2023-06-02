503a960980d3b8ceccd6df56a5d6488496b5437f
joouddo <>
2023-06-02 16:06:55 +0700

    Исправление padding

--- 04_Task/MST/Layers/MaxPool.py
+++ 04_Task/MST/Layers/MaxPool.py
@@ -12,9 +12,9 @@ class MaxPool2d(ConvBasicModule):
     def forward(self, x):
         BS, C, H, W = x.shape
         self._output_size = self._calculate_output_sizes(H, W)
-
-        self._inX = np.pad(x, [(0,0), (0,0), (self._padding[0], self._padding[0]), (self._padding[1], self._padding[1])], mode='constant', constant_values=self._padding_value)
-        self._inX_cols = self._inX.reshape(BS * C, 1, H, W)
+        self._inX = x
+        self._inX_padded = np.pad(x, [(0,0), (0,0), (self._padding[0], self._padding[0]), (self._padding[1], self._padding[1])], mode='constant', constant_values=self._padding_value)
+        self._inX_cols = self._inX_padded.reshape(BS * C, 1, H+self._padding[0]*2, W+self._padding[1]*2)
         self._inX_cols = self._im2col(self._inX_cols)  # .shape() = [C, K * K, BS * H * W]
 
         self._max_idx = np.argmax(self._inX_cols, axis=0) 
@@ -32,7 +32,7 @@ class MaxPool2d(ConvBasicModule):
         self._dinX = np.zeros_like(self._inX_cols)
         self._dinX[self._max_idx, range(self._max_idx.size)] = flatten_dOut
 
-        self._dinX = self._col2im(self._dinX, self._inX.shape)
+        self._dinX = self._col2im(self._dinX, self._inX_padded.shape)
         self._dinX.reshape(self._inX.shape)
 
         return self._dinX
